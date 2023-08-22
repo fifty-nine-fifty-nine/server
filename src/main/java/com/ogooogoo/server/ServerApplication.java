0b0c241c57d143a95d0db78e83d39cd8953211a6
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-08-22 19:42:29 +0900

    feat: Add HelloController to check Health

--- src/main/java/com/ogooogoo/server/ServerApplication.java
+++ src/main/java/com/ogooogoo/server/ServerApplication.java
@@ -2,6 +2,18 @@ package com.ogooogoo.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloController {
+
+	@GetMapping()
+	public String hello(){
+		return "helloword";
+	}
+	
+}
 
 @SpringBootApplication
 public class ServerApplication {
