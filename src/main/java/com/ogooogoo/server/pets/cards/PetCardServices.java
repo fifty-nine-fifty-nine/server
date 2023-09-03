273557f866976872c974e0c4cea3a6295e26f27d
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-09-03 16:07:58 +0900

    fix: Update Use All Args Constructor

--- src/main/java/com/ogooogoo/server/pets/cards/PetCardServices.java
+++ src/main/java/com/ogooogoo/server/pets/cards/PetCardServices.java
@@ -4,18 +4,15 @@ import com.ogooogoo.server.clients.dall_e.DallEClient;
 import com.ogooogoo.server.clients.dall_e.DallEResponse;
 import com.ogooogoo.server.clients.gpt.GptClient;
 import com.ogooogoo.server.clients.gpt.GptResult;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class PetCardServices {
     private final DallEClient dallEClient;
     private final GptClient gptClient;
-
-    public PetCardServices(DallEClient dallEClient, GptClient gptClient) {
-        this.dallEClient = dallEClient;
-        this.gptClient = gptClient;
-    }
-
+    
     public CreatePetCardResDto generate(CreatePetCardReqDto req) {
         GptResult gptResult = gptClient.getGptResponse(req.getDescription());
         DallEResponse dallEResponse = dallEClient.generateImage(gptResult);
