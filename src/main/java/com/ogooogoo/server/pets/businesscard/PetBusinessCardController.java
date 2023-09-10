f921e0b5d412d96c155835a64429293d1fae53f0
GiwoongBang <bgw6713@naver.com>
2023-09-10 22:03:39 +0900

    fix: change Mapping Url & add @PathVariable annotation

--- src/main/java/com/ogooogoo/server/pets/businesscard/PetBusinessCardController.java
+++ src/main/java/com/ogooogoo/server/pets/businesscard/PetBusinessCardController.java
@@ -14,22 +14,22 @@ public class PetBusinessCardController {
 
     private final PetBusinessCardService petBusinessCardService;
 
-    @PostMapping("/businesscards/{petBusinessCardId}")
+    @PostMapping("/businesscards")
     public ResponseEntity<?> createPetBusinessCard(@Validated @RequestBody PetBusinessCardRequestDto petBusinessCardRequestDto,
                                                    @AuthenticationPrincipal KakaoTokenInfo info) {
         return petBusinessCardService.createPetBusinessCard(petBusinessCardRequestDto, info);
     }
 
-    @PutMapping("/businesscards")
-    public ResponseEntity<?> updatePetBusinessCard(Long petBusinessCardId,
+    @PutMapping("/businesscards/{petBusinessCardId}")
+    public ResponseEntity<?> updatePetBusinessCard(@PathVariable Long petBusinessCardId,
                                                    @Validated @RequestBody PetBusinessCardRequestDto petBusinessCardRequestDto,
                                                    @AuthenticationPrincipal KakaoTokenInfo info) {
         return petBusinessCardService.updatePetBusinessCard(petBusinessCardId, petBusinessCardRequestDto, info);
     }
 
-    @DeleteMapping("/businesscards/")
-    public ResponseEntity<?> updatePetBusinessCard(Long petBusinessCardId,
-                                                   @AuthenticationPrincipal KakaoTokenInfo info) {
+    @DeleteMapping("/businesscards/{petBusinessCardId}")
+    public ResponseEntity<?> deleteBusinessCard(@PathVariable Long petBusinessCardId,
+                                                @AuthenticationPrincipal KakaoTokenInfo info) {
         return petBusinessCardService.deletePetBusinessCard(petBusinessCardId, info);
     }
 
