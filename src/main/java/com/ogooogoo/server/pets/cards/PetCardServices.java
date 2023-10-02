4fffe1b0da91f7f8c85774ae5e681a5ad6206b5f
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-10-02 18:08:57 +0900

    feat: Create Translate Logic With Papago

--- src/main/java/com/ogooogoo/server/pets/cards/PetCardServices.java
+++ src/main/java/com/ogooogoo/server/pets/cards/PetCardServices.java
@@ -4,6 +4,7 @@ import com.ogooogoo.server.clients.dall_e.DallEClient;
 import com.ogooogoo.server.clients.dall_e.DallEResponse;
 import com.ogooogoo.server.clients.gpt.GptClient;
 import com.ogooogoo.server.clients.gpt.GptResult;
+import com.ogooogoo.server.clients.naver.NaverClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,11 @@ public class PetCardServices {
     private final DallEClient dallEClient;
     private final GptClient gptClient;
 
+    private final NaverClient naverClient;
+
     public CreatePetCardResDto generate(CreatePetCardReqDto req) {
-        GptResult gptResult = gptClient.getGptResponse(req.getDescription());
+        String translatedText = naverClient.translateToEng(req.getDescription());
+        GptResult gptResult = gptClient.getGptResponse(translatedText);
         DallEResponse dallEResponse = dallEClient.generateImage(gptResult);
 
         String imgUrl = dallEResponse
